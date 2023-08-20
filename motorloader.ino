068204946af4545e16365c03f59e86a675eb9edf
paul <alpel@gmx.net>
2023-08-20 21:41:02 +0200

    done, servo stuff missing

--- motorloader.ino
+++ motorloader.ino
@@ -1,38 +1,221 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include "wifi_credentials.h" 
+// #include <Servo.h>
 
+// #define LED1 2
+// #define LED2 16
+
+const char* hostname1 = "motorloader";
 ESP8266WebServer server(80);
+const int MAX_VALUES = 5000;
+float csvValues[MAX_VALUES];
+int numValues = 0;
+bool runloop = false;
 
 void handleRoot() {
   String html = "<form action='/submit' method='POST'>";
   html += "CSV Data: <input type='text' name='csv_data'><br>";
   html += "<input type='submit' value='Submit'></form>";
+
+  html += "<form method='POST' action='/stop'>";
+  html += "<input type='submit' value='Stop Loop'></form>";
   server.send(200, "text/html", html);
 }
 
 void handleSubmit() {
+  // digitalWrite(LED2, HIGH);
+  // digitalWrite(LED1, LOW);
+
   String csvData = server.arg("csv_data");
   // Parse CSV data and store in an array
   // Provide feedback to the user
-  server.send(200, "text/plain", "CSV data received and processed.");
+  String t = "CSV Data received, processing...";
+  // server.send(200, "text/html ", t);
+  // Serial.println("sent: " + csvData);
+
+  // Parse CSV data and store in array
+  int startIndex = 0;
+  int endIndex = csvData.indexOf(',');
+  int valueIndex = 0;
+
+  bool err = false;
+
+  while (endIndex != -1 && valueIndex < MAX_VALUES && !err) {
+    String csvValue = csvData.substring(startIndex, endIndex);
+    // Serial.println(csvValue);
+    // Serial.println(csvValue.length() > 0);
+    // Serial.println(isFloat(csvValue));
+
+    if (csvValue.length() > 0) {
+      if(isFloat(csvValue)) {
+        csvValues[valueIndex] = csvValue.toFloat();
+      } else
+        err = true;
+    } else
+      err = true;
+    startIndex = endIndex + 1;
+    endIndex = csvData.indexOf(',', startIndex);
+    valueIndex++;
+  }
+
+  if (err) {
+    String t = "Error processing CSV data...";
+    t += "<form action='/'><input type='submit' value='Back to Form'></form>";
+    server.send(200, "text/html", t);
+    // digitalWrite(LED1, HIGH);
+    return;
+  }
+
+  // Store the last value
+  if (valueIndex < MAX_VALUES) {
+    String csvValue = csvData.substring(startIndex);
+    if (csvValue.length() > 0) {
+      if(isFloat(csvValue)) {
+        csvValues[valueIndex] = csvValue.toFloat();
+      } else
+        err = true;
+    } else
+      err = true;
+    valueIndex++;
+  } else {
+    String t = "Max number of values exceeded";
+    t += "<form action='/'><input type='submit' value='Back to Form'></form>";
+    server.send(200, "text/html", t);
+    // digitalWrite(LED1, HIGH);
+    return;
+  }
+
+  numValues = valueIndex;
+
+  if (err) {
+    String t = "Error processing CSV data...";
+    t += "<form action='/'><input type='submit' value='Back to Form'></form>";
+    server.send(200, "text/html", t);
+    // digitalWrite(LED1, HIGH);
+  } else {
+    String t = "Done processing CSV data...<br>";
+    t += "Number of values received: " + String(numValues) + "<br>";
+    t += "<form t action='/start' method='POST'>";
+    t += "Timestep(ms): <input type='text' name='timestep'><br>";
+    t += "<input type='submit' value='Start Simulation'></form>";
+    server.send(200, "text/html", t);
+  }
+  // digitalWrite(LED2, LOW);
+}
+
+bool isFloat(const String& str) {
+  for (size_t i = 0; i < str.length(); i++) {
+    char c = str.charAt(i);
+    if (isdigit(c) || c == '.' || c == '-' || c == '+') {
+      return true;
+    }
+  }
+  return false;
+}
+
+bool isInt(const String& str) {
+  for (size_t i = 0; i < str.length(); i++) {
+    char c = str.charAt(i);
+    if (!isdigit(c)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+void handleStop() {
+  runloop = false;
+  String t = "Stop Button was pressed.";
+  t += "<form action='/'><input type='submit' value='Back to Form'></form>";
+  server.send(200, "text/html", t);
+  // digitalWrite(LED1, HIGH);
+}
+
+void handleStart() {
+  String dTime = server.arg("timestep");
+  int timestep;
+  if (isInt(dTime)) {
+    timestep = dTime.toInt();
+  } else {
+    String t = "Error in timestep value!";
+    t += "<form action='/'><input type='submit' value='Back to Form'></form>";
+    server.send(200, "text/html", t);
+    // digitalWrite(LED1, HIGH);
+    return;
+  }
+
+  String t = "Running Simulation now";
+  t += "<form action='/'><input type='submit' value='Back to Form'></form>";
+  t += "<form action='/stop'><input type='submit' value='Stop Run'></form>";
+  server.send(200, "text/html", t);
+
+  runloop = true;
+  int ctr = 0;
+  while(runloop && ctr < numValues) {
+    // digitalWrite(LED2, digitalRead(LED2) ^ HIGH);
+    //servo write csvValues[ctr];
+    Serial.println(csvValues[ctr]);
+    server.handleClient();
+    delay(timestep);
+    ++ctr;
+  }
+  Serial.println("done");
 }
 
 void setup() {
+  // pinMode(LED1, OUTPUT);
+  // pinMode(LED2, OUTPUT);
+  // digitalWrite(LED1, LOW);
+  // digitalWrite(LED2, LOW);
+
+  delay(1000);
   Serial.begin(115200);
+  WiFi.hostname(hostname1);
   WiFi.begin(ssid, password);
+
+Serial.println("\nConnecting to WiFi");
   while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi...");
+    delay(500);
+    Serial.print(".");
+    // digitalWrite(LED1, digitalRead(LED1) ^ HIGH);
   }
-  Serial.println("Connected to WiFi");
+  Serial.println("\nConnected to WiFi");
+  Serial.print("Station IP Address: ");
+  Serial.println(WiFi.localIP());
 
   server.on("/", HTTP_GET, handleRoot);
   server.on("/submit", HTTP_POST, handleSubmit);
+  server.on("/stop", HTTP_GET, handleStop);
+  server.on("/start", HTTP_POST, handleStart);
 
-  server.begin();
+  // digitalWrite(LED1, LOW);  
 }
 
 void loop() {
   server.handleClient();
 }
+
+/*
+#include <Servo.h>
+
+byte servoPin = 9; // signal pin for the ESC.
+byte potentiometerPin = A0; // analog input pin for the potentiometer.
+Servo servo;
+
+void setup() {
+    servo.attach(servoPin);
+    servo.writeMicroseconds(1500); // send "stop" signal to ESC. Also necessary to arm the ESC.
+
+    delay(7000); // delay to allow the ESC to recognize the stopped signal.
+}
+
+void loop() {
+
+    int potVal = analogRead(potentiometerPin); // read input from potentiometer.
+
+    int pwmVal = map(potVal,0, 1023, 1100, 1900); // maps potentiometer values to PWM value.
+
+    servo.writeMicroseconds(pwmVal); // Send signal to ESC.
+
+*/

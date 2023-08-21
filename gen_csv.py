b63ed53ff30076b004f9067e5c755ccd018a7b7a
paul <alpel@gmx.net>
2023-08-21 20:22:25 +0200

    generators for data in python

---
+++ gen_csv.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+MAX_VALUES = 1200
+
+f = lambda x: abs(0.5*np.sin(x*2*np.pi/MAX_VALUES))+0.5
+
+res = [f(x) for x in range(0,MAX_VALUES)]
+
+plt.plot(res)
+plt.show()
+print(res)
+
+np.savetxt("res.txt", res, delimiter=",", newline=",", fmt="%.6f")
---
+++ gen_runStep.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+MAX_VALUES = 120
+
+f = lambda x: abs(0.5*np.sin(x*2*np.pi/MAX_VALUES))+0.5
+power = [f(x) for x in range(0,MAX_VALUES)]
+
+s = lambda x: (x**2)*75
+steps = [s(x) for x in np.linspace(-2,1,MAX_VALUES)]
+plt.plot(steps)
+plt.show()
+
+res = [None]*MAX_VALUES*2
+res[::2] = power
+res[1::2] = steps
+
+plt.plot(np.cumsum(res[1::2]), res[::2])
+plt.show()
+print(res)
+
+np.savetxt("res_step.txt", res, delimiter=",", newline=",", fmt="%.6f")
